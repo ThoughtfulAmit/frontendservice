0a90554bf5b386d0754f27f2fbe92d6d4079e094
stupidamigo <amit.sharma.tafs@gmail.com>
2021-04-14 22:22:35 +0000

    updated title

--- test/test-server.js
+++ test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1>Welcome - Bipin Deosthale Capstone Project</h1>"));
+        chai.assert.isTrue(res.text.includes("<h1>Welcome - DevOps Capstone Project</h1>"));
         return done();
       });
 
